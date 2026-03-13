9b08ec5baca06f10a6d1b10dc8b5c7e94c1f0efb
LuckyGuy <joseguest1337@gmail.com>
2026-03-12 21:29:56 -0700

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -18,7 +18,8 @@ haxelib install lime 8.1.2 --quiet
 haxelib install openfl 9.3.3 --quiet
 
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
+haxelib git linc_luajit https://github.com/AndreiRudenko/linc_luajit
+haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
 
